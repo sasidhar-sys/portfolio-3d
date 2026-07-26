ea379ba108b1f0a7b5348b90f3b40fc3dfe0f059
sasidhar-sys <mailtosasi.official@gmail.com>
2026-07-26 23:38:29 +0530

    chore: add supporting runtime assets and node helper

---
+++ node.js
@@ -0,0 +1,21 @@
+import fetch from "node-fetch";
+
+fetch("https://alche.studio/common/scene.glb", {
+  "headers": {
+    "accept": "*/*",
+    "accept-language": "en-US,en;q=0.9",
+    "cache-control": "no-cache",
+    "pragma": "no-cache",
+    "priority": "u=1, i",
+    "sec-ch-ua": "\"Not;A=Brand\";v=\"8\", \"Chromium\";v=\"150\", \"Google Chrome\";v=\"150\"",
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": "\"Windows\"",
+    "sec-fetch-dest": "empty",
+    "sec-fetch-mode": "cors",
+    "sec-fetch-site": "same-origin",
+    "cookie": "_ga=GA1.1.1707468661.1785041764; _ga_R00M7W3JTS=GS2.1.s1785082336$o4$g1$t1785083125$j60$l0$h0",
+    "Referer": "https://alche.studio/"
+  },
+  "body": null,
+  "method": "GET"
+});
